731c14875c4fd50ca8e37283accb34d704d55515
Nils_Laptop <104395997+Moviemonster@users.noreply.github.com>
2022-09-26 15:56:02 +0200

    Imported Exercise 2 Demographic Data Analyzer

--- FreeCodeCamp/Data Analysis with Python/Final Test Programms/Demographic-Data-Analyzer/main.py
+++ FreeCodeCamp/Data Analysis with Python/Final Test Programms/Demographic-Data-Analyzer/main.py
@@ -1,9 +1,9 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import mean_var_std
+import demographic_data_analyzer
 from unittest import main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
-#print(mean_var_std.calculate([0,1,2,3,4,5,6,7]))
+# Test your function by calling it here
+demographic_data_analyzer.calculate_demographic_data()
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
