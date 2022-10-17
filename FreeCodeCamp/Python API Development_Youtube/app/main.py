2108640c5629bcb74b8a92ee0f1c1742485228cf
Nils_PC <nils.bestehorn@gmail.com>
2022-10-17 19:58:18 +0200

    Video Cours API 06:18H creating Users

--- FreeCodeCamp/Python API Development_Youtube/app/main.py
+++ FreeCodeCamp/Python API Development_Youtube/app/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -7,8 +7,10 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session 
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
+
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -54,15 +56,15 @@ def root():
     return {"message": "Welcom to my API"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     #cursor.execute("""SELECT * FROM posts """)
     #posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return  posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     #cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING * """,
     #               (post.title, post.content, post.published))
@@ -75,10 +77,10 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return  new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     #cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
     #post = cursor.fetchone()
@@ -89,7 +91,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
         #Alternative
         #response.status_code = status.HTTP_404_NOT_FOUND
         #return {"message": f"post with id: {id} was not found"}
-    return {"post_detail": post}
+    return  post
 
 
 @app.delete("/posts/{id}")
@@ -110,7 +112,7 @@ def delete_post(id: int, status_code=status.HTTP_204_NO_CONTENT, db: Session = D
     return Response(status_code=status.HTTP_204_NO_CONTENT)
     
 
-@app.put("/posts/{id}")
+@app.put("/posts/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     #cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, 
     #               (post.title, post.content, post.published, str(id)))
@@ -123,7 +125,24 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
                             detail=f"post with id: {id} does not exist")
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post_query.first()}
+    return  post_query.first()
 
 
-    
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    #hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/users/{id}', response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
+    return user
--- FreeCodeCamp/Python API Development_Youtube/app/schemas.py
+++ FreeCodeCamp/Python API Development_Youtube/app/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
 
 #Schema of the BaseModel by pydabtic  
 class PostBase(BaseModel):
@@ -9,3 +11,21 @@ class PostBase(BaseModel):
     
 class PostCreate(PostBase):
     pass
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    class Config:
+        orm_mode=True
